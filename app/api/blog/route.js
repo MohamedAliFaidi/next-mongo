31cfda357564da5f4f574c4fea75d9e594a93658
MohamedAliFaidi <dalyyy3259@protonmail.com>
2023-06-21 16:37:14 +0100

    remove older git user

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -4,7 +4,7 @@ import connect from "@/lib/db.connect";
 // here you will define your blog api route
 // you can use req.json() to get the body data
 export async function POST(req) {
-
+ //
  // TODO get the body data
  //connect to the database
  //create a new blog
