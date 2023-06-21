64b9d7126693e5fdfb20327b15b3dd9cae0d7e04
dalyyy3259@protonmail.com <660daly@gmail.com>
2023-06-21 11:48:00 +0100

    init-workshop

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -4,9 +4,12 @@ import connect from "@/lib/db.connect";
 // here you will define your blog api route
 // you can use req.json() to get the body data
 export async function POST(req) {
+
  // TODO get the body data
  //connect to the database
  //create a new blog
   //save the blog
   //return the blog
+  // you can inpire from the login route thas is already defined in the app/(auth)/login/route.js
+  //note that when naming a page folder with parenthesis like this (auth) that means the path to that page will not include auth
 }
